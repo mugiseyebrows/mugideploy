53d2b5bd653bbe8f0e7dc07b413edd45bb5346de
Doronin Stanislav <mugisbrows@gmail.com>
2022-12-20 13:45:52 +0300

    0.0.17

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mugideploy",
-    version="0.0.16",
+    version="0.0.17",
     author="Doronin Stanislav",
     author_email="mugisbrows@gmail.com",
     url='https://github.com/mugiseyebrows/mugideploy',
